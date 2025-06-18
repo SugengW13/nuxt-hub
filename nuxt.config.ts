7cace3a05c21c5f8539ef84ceff2bafa653a7672
sugengw <sugeng.warsito@transtrack.id>
2025-06-18 14:12:22 +0700

    feat: init nuxt-hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxthub/core'],
   devtools: { enabled: true },
   devServer: { port: 3030 },
   compatibilityDate: '2025-05-15',
