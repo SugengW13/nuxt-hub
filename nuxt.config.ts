6eb6a26d569755dc086edfe6ac68de0739c7db77
sugengw <sugeng.warsito@transtrack.id>
2025-06-18 11:25:11 +0700

    feat: init eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
+  devServer: { port: 3030 },
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  eslint: {
+    config: { stylistic: true },
+  },
 })
